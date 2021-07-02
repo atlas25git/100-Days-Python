50801b89fbab03946e49bd3e5663808edd783b4f
atlas25git <atlas.sarthak@gmail.com>
2021-07-02 17:54:56 +0530

    Day25

---
+++ Day26/Comprehension.py
@@ -0,0 +1,21 @@
+numbers = [1,2,3]
+new_list = [n**7 for n in numbers]
+print(new_list)
+
+#this could also be used for strings or other containers
+
+numbers = [1,2,3]
+new_list = [n**7 for n in numbers if n!=2]
+print(new_list)
+
+#dictionaries
+import random
+names = ["A","B","c","D"]
+student_score={
+    student:random.randint(1,100) for student in names
+}
+
+print(student_score)
+
+passed = {student:score for (student,score) in student_score.items() if score>=60 }
+print(passed)
